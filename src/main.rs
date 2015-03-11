657a90b68989492e2087797611936200af1a31d7
Ivan Glushkov <ivan.glushkov@gmail.com>
2015-03-11 18:45:32 +0300

    correct memory management

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,6 @@
 mod resolver;
 
 fn main() {
-    println!("result:{:?}", resolver::resolver_lib::resolve("http://ya.ru"));
+    //println!("result:{:?}", resolver::resolver_lib::resolve("http://ya.ru"));
+    println!("result:{:?}", resolver::resolver_lib::resolve("http://bit.ly/1bh0k2I"));
 }
--- src/resolver/resolver_lib.rs
+++ src/resolver/resolver_lib.rs
@@ -6,33 +6,42 @@ use self::url::Url;
 use resolver::hyper_lib;
 
 #[derive(Debug)]
+struct Redirect (String, i32); // (url, http_code)
+#[derive(Debug)]
+struct Redirects ( Vec<Redirect> );
+
+#[derive(Debug)]
 struct Page {
     orig_url: String,
-    resolved_url: String,
-    title: String
+    resolved_url: Option<String>,
+    title: Option<String>,
+    redirects: Redirects
 }
 
-//#[derive(Debug)]
-//enum Result {
-//    ResolveResult(Page),
-//    Error(i32)
-//}
+pub fn resolve(x: &str) ->  Result<Box<Page>, String>{
+    let res = Box::new(Page {
+        orig_url: String::from_str(x),
+        resolved_url: None,
+        title: None,
+        redirects: Redirects(Vec::new())
+    });
+    resolve_ll(0, x, res)
+}
 
-//struct Redirect (String, i32); // (url, http_code)
-//struct Redirects ( Vec<Redirect> );
-
-pub fn resolve(x: &str) ->  Result<Page, String>{
+fn resolve_ll(attempt: u8, x: &str, mut res: Box<Page>) ->  Result<Box<Page>, String>{
     match Url::parse(x) {
         Ok(_) => {
             let (status, content_type, location, body) = hyper_lib::http_get(x);
 
-            println!(" resolve({}) -> status:{:?} ct:{:?} l:{:?} body:{}", 
-                x, status, content_type, location, body.len());
-            Ok(Page {
-                orig_url: String::from_str(""),
-                resolved_url: String::from_str(""), 
-                title: String::from_str("")
-            })},
+            let resolved_url = match status {
+                Some(200) => Some(String::from_str(x)), // ok
+                _ => None
+            };
+            println!(" resolve({}) -> at:{} status:{:?} ct:{:?} l:{:?} body:{}", 
+                x, attempt, status, content_type, location, body.len());
+            res.resolved_url = resolved_url;
+            Ok(res) 
+        },
         Err(e) => {
             let msg = format!("Malformed URL: {} ({})", x, e);
             println!("{}", msg);
